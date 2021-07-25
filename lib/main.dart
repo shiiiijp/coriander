7c00636cc372dbf660c7fd58665ca2a4e9b02061
Taishi Ito <itohlee14@gmail.com>
2021-07-25 09:22:06 +0900

    Revert "てすと"
    This reverts commit 3922d8d5

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
                 child: Column(
                   children: [
                     Text(
-                      model.sampleTextddaa,
+                      model.sampleText,
                       style: TextStyle(
                         fontSize: 30,
                       ),
